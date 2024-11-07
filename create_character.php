c59a6cb48beddb74db3138b1a773746c0d58eca4
mitnick <brunovega1985@gmail.com>
2024-11-07 13:18:23 +0100

    creando el form

---
+++ create_character.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>crea tu personaje</title>
+</head>
+<body>
+    <h1>Crea tu personaje</h1>
+    
+<form action="save_character.php" method ="POST">
+   
+    <label for="name">Name:</label>
+    <input  name ="name" type="text" id="name">
+
+    <label for="id">id:</label>
+    <input  name ="name" type="pasword" id="name">
+
+    <label for="name">Name:</label>
+    <input  name ="name" type="text" id="name">
+ 
+
+
+</form>
+
+</body>
+</html>
